c2a113054b8b43aa9e3fa54f1a4d2e847013878c
Alexander Horner <mail@alexanderhorner.com>
2022-07-05 08:44:07 +0200

    Mobile device compatible

--- hosting/src/views/HomeView.vue
+++ hosting/src/views/HomeView.vue
@@ -8,17 +8,31 @@ import PlayAudioButton from '@/components/PlayAudioButton.vue';
 
 const content = ref(null)
 
-const incompatibleDevice = ref(false)
+const responsiveAnimation = () => {
+  const aosElements = content.value.querySelectorAll('[data-aos]')
+
+  if (window.innerWidth < 1080) {
+    aosElements.forEach((element) => {
+      element.dataset.aosinitial = element.dataset.aos
+      element.dataset.aos = 'fade-up'
+    })
+  } else {
+    aosElements.forEach((element) => {
+      element.dataset.aos = element.dataset.aosinitial
+    })
+  }
+}
 
 onMounted(() => {
-  window.addEventListener('resize', () => {
-    if (window.innerWidth < 1000) {
-      incompatibleDevice.value = true
-    } else {
-      incompatibleDevice.value = false
-    }
+  const aosElements = content.value.querySelectorAll('[data-aos]')
+  aosElements.forEach((element) => {
+    element.dataset.aosinitial = element.dataset.aos
   })
-  
+  window.addEventListener('resize', responsiveAnimation)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', responsiveAnimation)
 })
 
 AOS.init({
@@ -42,57 +56,50 @@ const props = defineProps(['component'])
       </nav>
   </header>-->
 
-  <template v-if="incompatibleDevice">
-    <div style="text-align: center;">Dein Gerät ist zu klein</div>
-  </template>
+  <section name="top" class="hero-section">
+    <div class="titles" style="overflow: visible;">
+      <h1 class="title-main">Deutschlands Aufschwung</h1>
+      <div class="title-secondary">presented by History Extreme</div>
+    </div>
+  </section>
 
-  <template v-else>
+  <main ref="content" class="content">
 
-    <section name="top" class="hero-section">
-      <div class="titles" style="overflow: visible;">
-        <h1 class="title-main">Deutschlands Aufschwung</h1>
-        <div class="title-secondary">presented by History Extreme</div>
+    <!-- <component :is="component"></component> -->
+    <div>{{ component }}</div>
+
+    <section class="cdu-anti-kommunismus">
+      <img data-aos="fade-right" class="img-big " src="@/assets/img/cdu-anti-kommunismus.jpg" alt="Propagandafoto mit Text: CDU schützt vor Enteignung">
+      <div data-aos="fade-left" class="info">
+        <h2 class="info-headline">Anti Kommunismus</h2>
+        <PlayAudioButton></PlayAudioButton>
       </div>
     </section>
 
-    <main ref="content" class="content">
+    <!-- <img class="img-big name" src="@/assets/img/.jpg" alt="">
+    <div class="info name">
+      <h2 class="info-headline name">Anti Kommunismus</h2>
+      <PlayAudioButton></PlayAudioButton>
+    </div> -->
 
-      <!-- <component :is="component"></component> -->
-      <div>{{ component }}</div>
-
-      <section class="cdu-anti-kommunismus">
-        <img data-aos="fade-right" class="img-big " src="@/assets/img/cdu-anti-kommunismus.jpg" alt="Propagandafoto mit Text: CDU schützt vor Enteignung">
-        <div data-aos="fade-left" class="info">
-          <h2 class="info-headline">Anti Kommunismus</h2>
-          <PlayAudioButton></PlayAudioButton>
-        </div>
-      </section>
-
-      <!-- <img class="img-big name" src="@/assets/img/.jpg" alt="">
-      <div class="info name">
-        <h2 class="info-headline name">Anti Kommunismus</h2>
+    <section class="wirtschaftswunder">
+      <div data-aos="fade-right" class="info">
+        <h2 class="info-headline">Wirtschafts&shy;wunder</h2>
+        <div class="info-text" style="margin-bottom: 1rem">„Wohlstand für alle“ - Ludwig Erhard</div>
         <PlayAudioButton></PlayAudioButton>
-      </div> -->
+      </div>
+      <img data-aos="fade-left" class="img-big" src="@/assets/img/wirtschaftswunder.jpg" alt="">
+    </section>
 
-      <section class="wirtschaftswunder">
-        <div data-aos="fade-right" class="info">
-          <h2 class="info-headline">Wirtschafts&shy;wunder</h2>
-          <div class="info-text" style="margin-bottom: 1rem">„Wohlstand für alle“ - Ludwig Erhard</div>
-          <PlayAudioButton></PlayAudioButton>
-        </div>
-        <img data-aos="fade-left" class="img-big" src="@/assets/img/wirtschaftswunder.jpg" alt="">
-      </section>
+    <section class="währungsreform">
+      <img data-aos="fade-right" class="img-big" src="@/assets/img/währungsreform.jpg" alt="">
+      <div data-aos="fade-left" class="info">
+        <h2 class="info-headline">Währungs&shy;reform</h2>
+        <PlayAudioButton></PlayAudioButton>
+      </div>
+    </section>
 
-      <section class="währungsreform">
-        <img data-aos="fade-right" class="img-big" src="@/assets/img/währungsreform.jpg" alt="">
-        <div data-aos="fade-left" class="info">
-          <h2 class="info-headline">Währungs&shy;reform</h2>
-          <PlayAudioButton></PlayAudioButton>
-        </div>
-      </section>
-
-    </main>
-  </template>
+  </main>
 
   <RouterView />
 </template>
@@ -129,6 +136,7 @@ const props = defineProps(['component'])
   .titles {
     animation: 1s ease-out 0s 1 zoomIn;
     padding-bottom: 5vh;
+    margin: 0 2rem;
 
     .title-main {
       font-family: 'Playfair Display', serif;
@@ -151,6 +159,11 @@ const props = defineProps(['component'])
   grid-template-columns: 1fr repeat(5, 2fr) 1fr;
   row-gap: 150px;
   margin-top: min(100vh, 100vw);
+
+  @media screen and (max-width: 1080px) {
+     grid-template-columns: 20px repeat(5, 1fr) 20px;
+     row-gap: 20px;
+  }
 
   .img-big {
     width: 100%;
@@ -206,6 +219,21 @@ const props = defineProps(['component'])
       padding-right: 0;
     }
   }
+
+  section {
+    @media screen and (max-width: 1080px) {
+      img {
+        grid-column: 2 / span 5 !important;
+      }
+      .info {
+        grid-column: 2 / span 5 !important;
+        padding: 0;
+        padding-left: 0;
+        padding-right: 0;
+      }
+    }
+  }
+  
 
 }
 </style>
