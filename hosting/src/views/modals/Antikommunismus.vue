865c27c0bd3382b46b5eee80b3135130ca1e21e3
Alexander Horner <mail@alexanderhorner.com>
2022-07-10 18:49:16 +0200

    Change styles and add content

---
+++ hosting/src/views/modals/Antikommunismus.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TextModal from '@/components/TextModal.vue'
+import ReadMoreButton from '../../components/ReadMoreButton.vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <TextModal>
+        <h1>Antikommunismus</h1>
+
+        
+
+    </TextModal>
+</template>
